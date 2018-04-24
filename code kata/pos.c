574d0ffd708f3cffeb023a98fa8c70385aec76e5
saranyamvs <38651000+saranyamvs@users.noreply.github.com>
2018-04-24 14:11:06 +0530

    Create pos.c

---
+++ code kata/pos.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a<0)
+    {
+        printf("Negative");
+    }
+    else if(a==0)
+    {
+        printf("Zero");
+    }
+    else
+    {
+        printf("Positive");
+    }
+return 0;
+}
